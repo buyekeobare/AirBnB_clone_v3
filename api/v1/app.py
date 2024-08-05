30ecced4c47a38b259bde31ffc016d1db8043216
buyekeobare <buyekeobare@gmail.com>
2024-08-05 23:41:11 +0000

    files updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -28,8 +28,8 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(os.getenv('HBNB_API_PORT', 5000))
     # getenv returns a string and port is an int
     # THREADED is set to true so it can serve multiple requests at once
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=HOST, port=PORT, threaded=True)
