9046d7463c4966b835da38a8aeea07992bc1c3b7
buyekeobare <buyekeobare@gmail.com>
2024-08-06 03:29:59 +0000

    file updated

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -9,8 +9,7 @@ from models import storage
 from models.place import Place
 from models.review import Review
 
-@app_views.route('/places/<place_id>/reviews', methods=['GET'],
-                 strict_slashes=False)
+@app_views.route('/places/<place_id>/reviews', methods=['GET'], strict_slashes=False)
 def get_reviews_by_place(place_id):
     """Retrieve the list of all Review objects of a place"""
     place = storage.get("Place", place_id)
@@ -28,8 +27,7 @@ def get_review(review_id):
     return jsonify(review.to_dict())
 
 
-@app_views.route('/reviews/<review_id>', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/reviews/<review_id>', methods=['DELETE'], strict_slashes=False)
 def delete_review(review_id):
     """Delete a Review object by ID"""
     review = storage.get("Review", review_id)
@@ -40,8 +38,7 @@ def delete_review(review_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/places/<place_id>/reviews', methods=['POST'],
-                 strict_slashes=False)
+@app_views.route('/places/<place_id>/reviews', methods=['POST'], strict_slashes=False)
 def create_review(place_id):
     """Create a Review object"""
     place = storage.get("Place", place_id)
