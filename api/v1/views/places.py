ccaa90a45a8d318f0523b683f468a80f0230e241
buyekeobare <buyekeobare@gmail.com>
2024-08-06 03:33:17 +0000

    places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,8 +8,7 @@ from models import storage
 from models.city import City
 from models.place import Place
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'],
-                 strict_slashes=False)
+@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
 def get_places_by_city(city_id):
     """Retrieve the list of all Place objects of a city"""
     city = storage.get("City", city_id)
@@ -27,8 +26,7 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
 def delete_place(place_id):
     """Delete a Place object by ID"""
     place = storage.get("Place", place_id)
@@ -39,8 +37,7 @@ def delete_place(place_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'],
-                 strict_slashes=False)
+@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
 def create_place(city_id):
     """Create a Place object"""
     city = storage.get("City", city_id)
@@ -63,8 +60,7 @@ def create_place(city_id):
     return make_response(jsonify(place.to_dict()), 201)
 
 
-@app_views.route('/places/<place_id>', methods=['PUT'],
-                 strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 def update_place(place_id):
     """Update a Place object by ID"""
     place = storage.get("Place", place_id)
